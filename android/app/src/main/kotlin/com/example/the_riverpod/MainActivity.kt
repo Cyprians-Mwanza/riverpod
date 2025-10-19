fc5b5de52a16a27a1c115bc1b1593c571f67c69a
Cyprians Mwanza <cypriansmwanza@outlook.com>
2025-10-19 18:21:29 +0300

    set up

---
+++ android/app/src/main/kotlin/com/example/the_riverpod/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.the_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/models/note.dart
@@ -0,0 +1,33 @@
+import 'package:hive/hive.dart';
+import 'package:equatable/equatable.dart';
+
+part 'note.g.dart';
+
+@HiveType(typeId: 0)
+class Note extends HiveObject with EquatableMixin {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String title;
+
+  @HiveField(2)
+  final String body;
+
+  const Note({
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  Note copyWith({String? id, String? title, String? body}) {
+    return Note(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      body: body ?? this.body,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, title, body];
+}
